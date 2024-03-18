ed74a89c977d0ae94a6a59dd82608b1e1c0a9de0
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-18 16:36:30 +0200

    api service created

---
+++ client/src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Book } from "./types/book";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApiService {
+  api = "http://localhost:3030/data";
+  constructor(private http: HttpClient) {}
+
+  getBooks() {
+    const api = "http://localhost:3030/data";
+    return this.http.get<Book[]>(`${api}/books`);
+  }
+}
