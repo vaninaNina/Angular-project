b9c98d53a1a1f8f74eff73c0768ea8cbedf6dba2
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-24 20:20:31 +0200

    user router module for auth created

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { RegisterComponent } from "./user/register/register.component";
 import { MainComponent } from "./core/main/main.component";
-import { LoginComponent } from "./user/login/login.component";
 import { AllBooksComponent } from "./books/all-books/all-books.component";
 
 const routes: Routes = [
   { path: "", component: MainComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "login", component: LoginComponent },
+  // { path: "register", component: RegisterComponent },
+  // { path: "login", component: LoginComponent },
+  {
+    path: "auth",
+    loadChildren: () => import("./user/user.module").then((m) => m.UserModule),
+  },
   { path: "books", component: AllBooksComponent },
 ];
 
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -4,9 +4,18 @@ import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
+import { UserRoutingModule } from "./user-routing.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, SharedModule, FormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    UserRoutingModule,
+    RouterModule,
+    SharedModule,
+    FormsModule,
+  ],
 })
 export class UserModule {}
