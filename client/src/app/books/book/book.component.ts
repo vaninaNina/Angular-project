c5bec1509e3e44811fc8d9074a99d2603e0b3edf
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-25 21:43:43 +0200

    data received from the server added to book component

--- client/src/app/books/book/book.component.ts
+++ client/src/app/books/book/book.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "src/app/api.service";
+import { ActivatedRoute } from "@angular/router";
+import { Book } from "src/app/types/book";
 
 @Component({
-  selector: 'app-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  selector: "app-book",
+  templateUrl: "./book.component.html",
+  styleUrls: ["./book.component.css"],
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+  book = {} as Book;
+  constructor(
+    private apiService: ApiService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data["booksId"];
+
+      this.apiService.getBook(id).subscribe((book) => {
+        this.book = book;
+      });
+    });
+  }
 }
