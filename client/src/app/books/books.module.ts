60d4f180d4bfe777a40edef907a16788e313197a
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-25 20:38:58 +0200

    books module created

--- client/src/app/books/books.module.ts
+++ client/src/app/books/books.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AllBooksComponent } from "./all-books/all-books.component";
 import { BooksRoutingModule } from "./books-routing.module";
+import { BookComponent } from "./book/book.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [AllBooksComponent],
-  imports: [CommonModule, BooksRoutingModule],
+  declarations: [AllBooksComponent, BookComponent],
+  imports: [CommonModule, BooksRoutingModule, RouterModule],
 })
 export class BooksModule {}
