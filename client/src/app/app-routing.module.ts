bdb41913f261925dbebf44364f9323e772037fda
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-25 20:38:40 +0200

    books component added to the router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "./core/main/main.component";
 import { AllBooksComponent } from "./books/all-books/all-books.component";
+import { BookComponent } from "./books/book/book.component";
+import { AllPostsComponent } from "./posts/all-posts/all-posts.component";
 
 const routes: Routes = [
   { path: "", component: MainComponent },
@@ -12,6 +14,8 @@ const routes: Routes = [
     loadChildren: () => import("./user/user.module").then((m) => m.UserModule),
   },
   { path: "books", component: AllBooksComponent },
+  { path: "books/details/:booksId", component: BookComponent },
+  { path: "posts", component: AllPostsComponent },
 ];
 
 @NgModule({
