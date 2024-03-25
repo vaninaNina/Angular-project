139bcb596270af8e48c6f368cc50f7ed4e16b9a4
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-25 20:39:36 +0200

    books and book details routes created

--- client/src/app/books/books-routing.module.ts
+++ client/src/app/books/books-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes } from "@angular/router";
 import { AllBooksComponent } from "./all-books/all-books.component";
+import { BookComponent } from "./book/book.component";
 
 const routes: Routes = [
   {
     path: "books",
     component: AllBooksComponent,
   },
+  { path: ":bookId", component: BookComponent },
 ];
 
 @NgModule({
