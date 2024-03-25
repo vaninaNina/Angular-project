622423655cbc0bbd1ce68c4404f8799b67ea8f03
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-25 20:37:28 +0200

    posts module added

---
+++ client/src/app/posts/posts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllPostsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostsModule { }
