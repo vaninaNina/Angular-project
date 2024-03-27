46efa6d321004ef73601abc7ae837b5a4065a1c3
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-27 22:57:17 +0200

    post component, routing and module added imported

--- client/src/app/posts/posts.module.ts
+++ client/src/app/posts/posts.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AllPostsComponent } from './all-posts/all-posts.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AllPostsComponent } from "./all-posts/all-posts.component";
+import { PostComponent } from "./post/post.component";
+import { PostsRoutingModule } from "./posts-routing.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [
-    AllPostsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AllPostsComponent, PostComponent],
+  imports: [CommonModule, PostsRoutingModule, RouterModule],
 })
-export class PostsModule { }
+export class PostsModule {}
