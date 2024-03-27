c1c80e469c1c0732e468f9286d531b13663fd35b
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-27 22:55:12 +0200

    all posts data added to the component

--- client/src/app/posts/all-posts/all-posts.component.ts
+++ client/src/app/posts/all-posts/all-posts.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "src/app/api.service";
+import { Post } from "src/app/types/post";
 
 @Component({
-  selector: 'app-all-posts',
-  templateUrl: './all-posts.component.html',
-  styleUrls: ['./all-posts.component.css']
+  selector: "app-all-posts",
+  templateUrl: "./all-posts.component.html",
+  styleUrls: ["./all-posts.component.css"],
 })
-export class AllPostsComponent {
+export class AllPostsComponent implements OnInit {
+  posts: Post[] = [];
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+      },
+      error: (err) => {
+        console.log("Error: ", err);
+      },
+    });
+  }
 }
