d1f3b4e9aca8583d02b0bbf19285db43d39a4bb2
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-27 13:17:40 +0200

    save button changed to be only visible for users

--- client/src/app/books/book/book.component.ts
+++ client/src/app/books/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "src/app/api.service";
 import { ActivatedRoute } from "@angular/router";
 import { Book } from "src/app/types/book";
+import { UserService } from "src/app/user/user.service";
 
 @Component({
   selector: "app-book",
@@ -12,9 +13,13 @@ export class BookComponent implements OnInit {
   book = {} as Book;
   constructor(
     private apiService: ApiService,
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private userService: UserService
   ) {}
 
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
   ngOnInit(): void {
     this.activeRoute.params.subscribe((data) => {
       const id = data["booksId"];
