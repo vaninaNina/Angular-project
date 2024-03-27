0e5e01eda2cd72eeede9815de4301950d3f86c6b
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-27 13:05:07 +0200

    my profile component created

--- client/src/app/types/user.ts
+++ client/src/app/types/user.ts
@@ -1,10 +1,12 @@
 export interface UserForAuth {
+  username: string;
   _id: string;
   email: string;
   password: string;
 }
 
 export interface Profile {
+  username: string;
   email: string;
   savedBooks: string[];
   posts: string[];
--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../user.service";
+import { Profile } from "src/app/types/user";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profile: Profile = {
+    username: "",
+    email: "",
+    savedBooks: [],
+    posts: [],
+  };
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    const { email, username } = this.userService.user!;
+    console.log(email, username);
+    // this.profile = {
+    //   username,
+    //   email,
+    // };
+  }
 }
