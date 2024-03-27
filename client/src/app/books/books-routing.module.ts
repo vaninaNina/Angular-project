8ab545e1fe3c59cd5896d444b222c1244b971d3a
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-27 21:46:26 +0200

    book routes updated

--- client/src/app/books/books-routing.module.ts
+++ client/src/app/books/books-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { Routes } from "@angular/router";
+import { Router, RouterModule, Routes } from "@angular/router";
 import { AllBooksComponent } from "./all-books/all-books.component";
 import { BookComponent } from "./book/book.component";
 
 const routes: Routes = [
   {
-    path: "books",
+    path: "",
     component: AllBooksComponent,
   },
   { path: ":bookId", component: BookComponent },
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class BooksRoutingModule {}
