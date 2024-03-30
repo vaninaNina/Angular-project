4961f343502596bf79af640a782d7d21714d412a
vaninaNina <kalapsazova.vanina@gmail.com>
2024-03-30 12:39:46 +0200

    declarations and imports updated on user module

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { SharedModule } from "../shared/shared.module";
 import { UserRoutingModule } from "./user-routing.module";
 import { RouterModule } from "@angular/router";
 import { ProfileComponent } from "./profile/profile.component";
+import { AuthenticateComponent } from "./authenticate/authenticate.component";
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent, ProfileComponent],
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    AuthenticateComponent,
+  ],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     UserRoutingModule,
     RouterModule,
     SharedModule,
