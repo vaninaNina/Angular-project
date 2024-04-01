5023ca0df4f2005d45107650e87c46e2bafcd619
vaninaNina <kalapsazova.vanina@gmail.com>
2024-04-01 12:50:00 +0300

    route guard for profile page added

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { AuthGuard } from "../guards/auth.guard";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent },
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
